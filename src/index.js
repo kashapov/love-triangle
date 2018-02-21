942ff89ecd88de0b2069abb4c58573da64bc39b0
unknown <akashapov@ES-136.ELECTROSERVICE>
2018-02-21 15:20:19 +0300

    Love triangles counter v.1.0

--- src/index.js
+++ src/index.js
@@ -3,5 +3,29 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var cntLoveTriangles = 0;
+      
+  var firstLover;
+  var secondLover;  
+  
+  var loverSearch = 3;
+  
+  for (var i = 0; i < preferences.length; i++) {
+    firstLover = preferences[i];
+    secondLover = preferences[firstLover-1];
+    
+    //console.log('i='+i+',firstLover='+firstLover+',secondLover='+secondLover);
+
+    if (preferences[secondLover-1] == i+1) {
+      cntLoveTriangles++;
+    }
+  }
+  
+  cntLoveTriangles = Math.floor(cntLoveTriangles/loverSearch);
+
+  return cntLoveTriangles;
 };
+
+let count = getLoveTrianglesCount([2, 3, 1]);
+  console.log(count); // 1
+ 
